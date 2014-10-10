424d6905a4ff276d34ba7348abd01e7d54ebad90
AndreaM19 <19andrea.m@gmail.com>
2014-10-10 16:09:38 +0200

    Corretto errore sulla struttura HTML

--- src/it/univpm/idstid/openstack/network/client/OpenstackProject.java
+++ src/it/univpm/idstid/openstack/network/client/OpenstackProject.java
@@ -2,9 +2,10 @@ package it.univpm.idstid.openstack.network.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 /**
@@ -29,7 +30,19 @@ public class OpenstackProject implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
-		
+		// Create a Label and an HTML widget.
+		Label lbl = new Label("This is just text.  It will not be interpreted "
+				+ "as <html>.");
+
+		HTML html = new HTML(
+				"This is <b>HTML</b>.  It will be interpreted as such if you specify "
+						+ "the <span style='font-family:fixed'>asHTML</span> flag.", true);
+
+		// Add them to the root panel.
+		VerticalPanel panel = new VerticalPanel();
+		panel.add(lbl);
+		panel.add(html);
+		RootPanel.get("contentSection").add(panel);
+
 	}
 }
