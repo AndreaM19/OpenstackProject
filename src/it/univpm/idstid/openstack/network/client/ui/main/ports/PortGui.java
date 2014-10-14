9f064be3c28fdce89a22ff2964f609ab96523b8e
righi21 <righi21@gmail.com>
2014-10-14 14:02:19 +0200

    Completate le UI per create e update Port

--- src/it/univpm/idstid/openstack/network/client/ui/main/ports/PortGui.java
+++ src/it/univpm/idstid/openstack/network/client/ui/main/ports/PortGui.java
@@ -2,6 +2,9 @@ package it.univpm.idstid.openstack.network.client.ui.main.ports;
 
 import it.univpm.idstid.openstack.network.var.OpenstackNetConstants;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.AbsolutePanel;
@@ -11,60 +14,119 @@ import com.google.gwt.user.client.ui.TextBox;
 public class PortGui  extends AbsolutePanel{
 	private HTML br = new HTML("<br>", true);
 	private HTML pageTitle=new HTML();
-	
+
 	public PortGui(){
 		this.setSize("900px", "700px");
-//		this.clear();
-		//add Ports UI elements
+		//		this.clear();
+		
 		pageTitle.setHTML(OpenstackNetConstants.HTML_PAGE_TITLE_PORT);
 		this.add(pageTitle,0,0);
-		
+
+	}
+
+	public void createPortUi(){
+		this.clear();
+		pageTitle.setHTML(OpenstackNetConstants.HTML_PAGE_TITLE_PORT_CREATE);
+		this.add(pageTitle,0,0);
+
 		//Admin State Up
 		Label adminLabel = new Label(OpenstackNetConstants.LABEL_ADMINSTATEUP);
 		this.add(adminLabel,0,100);
 		CheckBox adminCheck = new CheckBox();
 		this.add(adminCheck, 150,100);
-				
+
 		//Device Id
 		Label deviceLabel = new Label(OpenstackNetConstants.LABEL_DEVICEID);
 		this.add(deviceLabel,0,140);
 		TextBox deviceText = new TextBox();
 		this.add(deviceText,150,140);
-		
+
 		//Name
 		Label nameLabel = new Label(OpenstackNetConstants.LABEL_NAME);
 		this.add(nameLabel,0,180);
-		
+		TextBox nameText = new TextBox();
+		this.add(nameText,150,180);
+
 		//Network Id
 		Label networkLabel = new Label(OpenstackNetConstants.LABEL_NETWORKID);
 		this.add(networkLabel,0,220);
-		
+		TextBox networkText = new TextBox();
+		this.add(networkText,150,220);
+
 		//Security groups
 		Label securityLabel = new Label(OpenstackNetConstants.LABEL_SECURITYGROUPS);
 		this.add(securityLabel,0,260);
-		
-		
-				
+		TextBox securityText = new TextBox();
+		this.add(securityText,150,260);
+
+		// Add Cancel and Create button
+		Button cancelButton = new Button("Cancel", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			}
+		});
+		this.add(cancelButton,300,340);
+
+		Button createButton = new Button("Create", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			}
+		});
+		this.add(createButton,365,340);
+
+
 	}
-	
-	public void createPortUi(){
-		this.clear();
-		//add Ports UI elements
-	}
-	
+
 	public void createMultiplePortUi(){
 		this.clear();
 		//add Ports UI elements
 	}
-	
+
 	public void showPortUi(){
 		this.clear();
 		//add Ports UI elements
 	}
-	
+
 	public void updatePortUi(){
 		this.clear();
-		//add Ports UI elements
+		
+		pageTitle.setHTML(OpenstackNetConstants.HTML_PAGE_TITLE_PORT_UPDATE);
+		this.add(pageTitle,0,0);
+
+		//Port Name
+		Label portLabel = new Label(OpenstackNetConstants.LABEL_PORTNAME);
+		this.add(portLabel,0,100);
+		TextBox portText = new TextBox();
+		this.add(portText,150,100);
+
+		//Security groups
+		Label securityLabel = new Label(OpenstackNetConstants.LABEL_SECURITYGROUPS);
+		this.add(securityLabel,0,140);
+		TextBox securityText = new TextBox();
+		this.add(securityText,150,140);
+		
+		//Ip address
+		Label ipLabel = new Label(OpenstackNetConstants.LABEL_IPADDRESS);
+		this.add(ipLabel,0,180);
+		TextBox ipText = new TextBox();
+		this.add(ipText,150,180);
+
+		//Admin State Up
+		Label adminLabel = new Label(OpenstackNetConstants.LABEL_ADMINSTATEUP);
+		this.add(adminLabel,0,220);
+		CheckBox adminCheck = new CheckBox();
+		this.add(adminCheck, 150,220);
+
+		// Add Cancel and Update button
+		Button cancelButton = new Button("Cancel", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			}
+		});
+		this.add(cancelButton,295,340);
+
+		Button updateButton = new Button("Update", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			}
+		});
+		this.add(updateButton,360,340);
 	}
 
 }
--- src/it/univpm/idstid/openstack/network/var/OpenstackNetConstants.java
+++ src/it/univpm/idstid/openstack/network/var/OpenstackNetConstants.java
@@ -17,6 +17,8 @@ public class OpenstackNetConstants {
 	public static final String PAGE_LABEL_PORT="Ports";
 	public static final String PAGE_LABEL_QUOTA="Quotas";
 	public static final String PAGE_LABEL_OVERVIEW="Overview";
+	public static final String PAGE_LABEL_PORT_CREATE="Ports - Create port";
+	public static final String PAGE_LABEL_PORT_UPDATE="Ports - Update port";
 	
 	//HTML Elements
 	public static final String HTML_PAGE_TITLE_NETWORK="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_NETWORK+"</h3>";
@@ -24,6 +26,8 @@ public class OpenstackNetConstants {
 	public static final String HTML_PAGE_TITLE_PORT="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_PORT+"</h3>";
 	public static final String HTML_PAGE_TITLE_QUOTA="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_QUOTA+"</h3>";
 	public static final String HTML_PAGE_TITLE_OVERVIEW="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_OVERVIEW+"</h3>";
+	public static final String HTML_PAGE_TITLE_PORT_UPDATE="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_PORT_UPDATE+"</h3>";
+	public static final String HTML_PAGE_TITLE_PORT_CREATE="<h3 id='sectionTitle' class='section-title'>"+OpenstackNetConstants.PAGE_LABEL_PORT_CREATE+"</h3>";
 		
 	public static final String HTML_OVERVIEW_1="<p><br>OVERVIEW NEUTRON PROJECT <br>This section shows the Openstack network functionalities, whose architecture has been renamed Neutron.<br>From here you can create a virtual network and interact with to manage subnets and ports associated with them.<br>Only to admin is given the possibility to access some functions, like the ability to define the quotas values ​​and other extended features.<br>Available functions are related to the macro-areas:<br></p>";
 	public static final String HTML_OVERVIEW_2="<p>NETWORK: In Openstack a network is an isolated virtual layer-2 broadcast domain that is typically reserved for the tenant who created it unless the network is configured to be shared. Tenants can create multiple networks until they reach the thresholds specified by per-tenant quotas.<br>In the Networking API v2.0, the network is the main entity. Ports and subnets are always associated with a network.</p>";
@@ -35,15 +39,14 @@ public class OpenstackNetConstants {
 	public static final String LABEL_NAME="Name";
 	public static final String LABEL_NETWORKID="Network Id";
 	public static final String LABEL_SECURITYGROUPS="Security groups";
+	public static final String LABEL_SUBNETS_ASSOCIATED="Subnets associated";
+	public static final String LABEL_SHARED="Shared";
+	public static final String LABEL_STATUS="Status";
+	public static final String LABEL_ACTIONS="Actions";
+	public static final String LABEL_IPADDRESS="IP Address";
+	public static final String LABEL_PORTNAME="Port name";
 	
-	
-//Available functions are:
-// - List Networks
-// - Create network
-// - Create multiple networks
-// - Show network
-// - Update network
-// - Delete network
-// ";
-	
+	//Buttons
+	public static final String BUTTON_NETWORK_CREATE="Create network";
+	public static final String BUTTON_NETWORK_CREATE_MULTIPLE="Create multiple network";
 }
